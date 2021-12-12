1599beaa4517d6eb8973bd2d0c37b89f38ed33c9
Alexsander Uglin <uglin76@mail.ru>
2021-12-12 06:01:25 +0300

    refactor: total

--- main.py
+++ main.py
@@ -2,36 +2,59 @@ import asyncio
 import aiosqlite
 from aiosmtplib import send
 import more_itertools
+from email.message import EmailMessage
+import os
+from dotenv import load_dotenv
 
-HOSTNAME = 'smtp.mail.ru'
-PORT = 587
-SENDER = 'username@mail.ru'
-USERNAME = 'username'
-PASSWORD = 'password'
 
-async def get_and_send():
+async def get_data(sender, hostname, port, username, password):
     messages = []
+
     async with aiosqlite.connect('data/contacts.db') as db:
         async with db.execute("SELECT first_name, email FROM contacts") as cursor:
             async for row in cursor:
+
                 name, email = row[0], row[1]
-                message = f'Dear {name}!\nThank you for using our classifieds service.'
+
+                message = EmailMessage()
+                message["From"] = sender
+                message["To"] = email
+                message["Subject"] = 'Hello from spam bot!'
+                message.set_content(f'Dear {name}!\nThank you for using our classifieds service.')
+
                 messages.append(
                     send(
                         message,
-                        recipients=email,
-                        sender=SENDER,
-                        hostname=HOSTNAME,
-                        port=PORT,
-                        username=USERNAME,
-                        password=PASSWORD,
+                        hostname=hostname,
+                        port=port,
+                        username=username,
+                        password=password,
                         start_tls=True
                     )
                 )
 
-                for chunk in more_itertools.chunked(messages, 10):
-                    await asyncio.gather(*chunk)
+    return messages
+
+
+async def main(params):
+    data = await get_data(**params)
+
+    for chunk in more_itertools.chunked(data, 10):
+        try:
+            await asyncio.gather(*chunk, return_exceptions=False)
+        except Exception as e:
+            print(f'[ERROR] {e}')
+
 
 if __name__ == '__main__':
-    asyncio.run(get_and_send())
+    load_dotenv()
 
+    config = {
+        'hostname': os.getenv('HOSTNAME'),
+        'port': os.getenv('PORT'),
+        'sender': os.getenv('SENDER'),
+        'username': os.getenv('USER'),
+        'password': os.getenv('PASSWORD'),
+    }
+
+    asyncio.run(main(config))
