ac31427a1473b0592ca19879345363538d3fd5dc
Alexsander Uglin <uglin76@mail.ru>
2021-12-13 11:56:12 +0300

    refactor: total #2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 import aiosqlite
-from aiosmtplib import send
+import aiosmtplib
 import more_itertools
 from email.message import EmailMessage
 import os
@@ -9,14 +9,26 @@ from dotenv import load_dotenv
 """
 С ошибками, о которых писал в комментариях к работе, справился.
 Но возникли новые вопросы:
-Как отслеживать выполнение задач? 
-Письмо отправилось/не отправилось, как получить сообщение об этом?
 Как прикрутить прогресс бар?
 """
 
+async def send_mail(hostname, port, username, password, message):
+    smtp = aiosmtplib.SMTP(hostname=hostname, port=port)
+    await smtp.connect()
+    await smtp.starttls()
+    await smtp.login(username, password)
+
+    try:
+        await smtp.send_message(message)
+        print(f'send email to {message["To"]}')
+    except Exception as e:
+        print(f'[ERROR] {e}')
+
+    await smtp.quit()
+
 
 async def get_data(sender, hostname, port, username, password):
-    messages = []
+    coros = []
 
     async with aiosqlite.connect('data/contacts.db') as db:
         async with db.execute("SELECT first_name, email FROM contacts") as cursor:
@@ -30,28 +42,15 @@ async def get_data(sender, hostname, port, username, password):
                 message["Subject"] = 'Hello from spam bot!'
                 message.set_content(f'Dear {name}!\nThank you for using our classifieds service.')
 
-                messages.append(
-                    send(
-                        message,
-                        hostname=hostname,
-                        port=port,
-                        username=username,
-                        password=password,
-                        start_tls=True
-                    )
-                )
+                coros.append(send_mail(hostname, port, username, password, message))
 
-    return messages
+    return coros
 
 
 async def main(params):
     data = await get_data(**params)
-
     for chunk in more_itertools.chunked(data, 10):
-        try:
-            await asyncio.gather(*chunk, return_exceptions=False)
-        except Exception as e:
-            print(f'[ERROR] {e}')
+        await asyncio.gather(*chunk, return_exceptions=False)
 
 
 if __name__ == '__main__':
